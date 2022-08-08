a4116a4e5ab3116cb779572497b3f0c43a717ed0
Brad Jenkins <brad@pop-os.localdomain>
2022-08-08 18:18:49 -0400

    Main functionality commit, all features present are working as intended.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var info = {
     id: 510208,
-    range: 0,
-    selected: 0
+    range: null,
+    playing: null,
+    autoplayEnabled: true
 };
 
 let getRange = (last = 0, current = 500) => {
@@ -23,18 +24,51 @@ let getRange = (last = 0, current = 500) => {
 }
 
 let main = async () => {
-    let link, data;
-    await getRange();
-    console.log(info);
+    let link, src;
 
-    await fetch("https://www.npr.org/get/" + info.id + "/render/partial/next?start=" + info.selected)
+    await fetch("https://www.npr.org/get/" + info.id + "/render/partial/next?start=" + info.playing)
         .then(response => response.text())
         .then(data => {
             let doc = new DOMParser().parseFromString(data, "text/html");
-            link = doc.querySelector("a.audio-module-listen").href;
+            let embed = new DOMParser().parseFromString(doc.querySelector("b.embed-url > code").innerText, "text/html");
+            link = embed.querySelector("iframe").src;
+            document.getElementById("episode-title").innerText = doc.querySelector(".title > a").innerText;
         });
 
-    console.log(link);
+    fetch(link)
+        .then(response => response.text())
+        .then(data => {
+            doc = new DOMParser().parseFromString(data, "text/html");
+            let script = doc.querySelector("main script").innerText.replace("var apiDoc = ", "");
+            let startIndex = script.indexOf("https:\\/\\/ondemand.npr.org\\/");
+            let stopIndex = script.indexOf("\"", startIndex);
+            src = script.substring(startIndex, stopIndex).replaceAll("\\", "");
+            document.querySelector("#player > audio").src = src;
+        })
 }
 
-document.addEventListener("DOMContentLoaded", main);
+let update = () => {
+    info.id = document.getElementById("podcast-id").value;
+    info.playing = info.range - document.getElementById("episode-number").value + 1;
+    info.autoplayEnabled = document.getElementById("autoplay-enabled").checked;
+    main();
+}
+
+let autoplay = () => {
+    if (info.autoplayEnabled && info.playing > 1) {
+        info.playing--;
+        document.getElementById("episode-number").value = info.range - info.playing + 1;
+        main();
+    }
+}
+
+let setup = async () => {
+    await getRange();
+    document.getElementById("episode-range-label").innerText = " (1-" + info.range + ")";
+    document.getElementById("episode-number").max = info.range;
+}
+
+document.addEventListener("DOMContentLoaded", setup);
+document.getElementById("update-button").addEventListener("click", update);
+document.getElementById("update-button").addEventListener("touchstart", update);
+document.getElementById("audio").addEventListener("ended", autoplay);
